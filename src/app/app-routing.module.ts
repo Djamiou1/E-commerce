ab4ca149a23e1acc8a762f3c6af495cf608e3298
djamiou <ydjamiou216@gmail.com>
2023-02-01 17:41:28 +0100

    style

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { ProduitComponent } from './produit/produit.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    ProduitComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModuleModule { }
